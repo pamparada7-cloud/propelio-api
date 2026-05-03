e936aa7ad56fc16a10d40aa2048e9ab0122053b2
pamparada7-cloud <pamparada7@gmail.com>
2026-05-03 18:16:23 -0300

    Update generate-description.ts

--- api/generate-description.ts
+++ api/generate-description.ts
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
     const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
 
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${GEMINI_API_KEY}`,
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${GEMINI_API_KEY}`,
       {
         method: "POST",
         headers: {
@@ -27,11 +27,15 @@ export default async function handler(req, res) {
 
     const data = await response.json();
 
-    const text =
-      data.candidates?.[0]?.content?.parts?.[0]?.text ||
-      "Error generando texto";
+console.log("RESPONSE GEMINI:", data);
 
-    res.status(200).json({ text });
+if (!data.candidates) {
+  return res.status(500).json({ error: data });
+}
+
+const text = data.candidates[0].content.parts[0].text;
+
+res.status(200).json({ text });
 
   } catch (error) {
     res.status(500).json({ error: error.message });
